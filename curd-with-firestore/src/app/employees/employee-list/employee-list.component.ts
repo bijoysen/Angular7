577bd944a3d328be98c4c5708aaceb530a3694cf
bijoysen <bjoysen@gmail.com>
2019-01-23 19:47:28 +0530

    User delete function implemented

--- curd-with-firestore/src/app/employees/employee-list/employee-list.component.ts
+++ curd-with-firestore/src/app/employees/employee-list/employee-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { EmployeeService } from 'src/app/shared/employee.service';
 import { Employee } from 'src/app/shared/employee.model';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-employee-list',
@@ -10,7 +12,7 @@ import { Employee } from 'src/app/shared/employee.model';
 export class EmployeeListComponent implements OnInit {
 
   list: Employee[];
-  constructor( private service: EmployeeService) { }
+  constructor( private service: EmployeeService, private firestore: AngularFirestore, private toastr: ToastrService) { }
 
   ngOnInit() {
     this.service.getEmployees().subscribe(actionArray => {
@@ -27,4 +29,10 @@ export class EmployeeListComponent implements OnInit {
     this.service.formData = Object.assign({}, emp);
   }
 
+  OnDelete(id: string) {
+    if (confirm('Are you sure to delete this record')) {
+      this.firestore.doc('employees/' + id).delete();
+      this.toastr.warning('Deleted successfully', 'EMP. Register');
+     }
+  }
 }
