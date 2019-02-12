d1534227f7e46aa568d617efec5e4d0192e98ba7
bijoysen <bjoysen@gmail.com>
2019-02-12 16:21:34 +0530

    Router defined and linked

--- photoPost/src/app/app-routing.module.ts
+++ photoPost/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AllPostsComponent } from './all-posts/all-posts.component';
+import { FollowingComponent } from './following/following.component';
+import { FavouritesComponent } from './favourites/favourites.component';
+import { MyPostComponent } from './my-post/my-post.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent },
+  {path: 'allpost', component: AllPostsComponent },
+  {path: 'following', component: FollowingComponent },
+  {path: 'favourites', component: FavouritesComponent },
+  {path: 'my-posts', component: MyPostComponent }
+  // {path: '', component: }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
