1a1d57017104496716a9baac653306ca59223460
bijoysen <bjoysen@gmail.com>
2019-02-12 16:31:43 +0530

    Register and Login routing done

--- photoPost/src/app/app-routing.module.ts
+++ photoPost/src/app/app-routing.module.ts
@@ -5,14 +5,17 @@ import { AllPostsComponent } from './all-posts/all-posts.component';
 import { FollowingComponent } from './following/following.component';
 import { FavouritesComponent } from './favourites/favourites.component';
 import { MyPostComponent } from './my-post/my-post.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { LoginComponent } from './auth/login/login.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent },
   {path: 'allpost', component: AllPostsComponent },
   {path: 'following', component: FollowingComponent },
   {path: 'favourites', component: FavouritesComponent },
-  {path: 'my-posts', component: MyPostComponent }
-  // {path: '', component: }
+  {path: 'my-posts', component: MyPostComponent },
+  {path: 'register', component: RegisterComponent},
+  {path: 'login', component: LoginComponent }
 ];
 
 @NgModule({
