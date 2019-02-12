1565a46caa51587087cfbb2f9b187a0fee02e2d3
bijoysen <bjoysen@gmail.com>
2019-02-12 16:02:20 +0530

    All component added

--- photoPost/src/app/app.module.ts
+++ photoPost/src/app/app.module.ts
@@ -4,11 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { AllPostsComponent } from './all-posts/all-posts.component';
+import { FollowingComponent } from './following/following.component';
+import { FavouritesComponent } from './favourites/favourites.component';
+import { MyPostComponent } from './my-post/my-post.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { LoginComponent } from './auth/login/login.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    AllPostsComponent,
+    FollowingComponent,
+    FavouritesComponent,
+    MyPostComponent,
+    RegisterComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ photoPost/src/app/my-post/my-post.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-post',
+  templateUrl: './my-post.component.html',
+  styleUrls: ['./my-post.component.scss']
+})
+export class MyPostComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
