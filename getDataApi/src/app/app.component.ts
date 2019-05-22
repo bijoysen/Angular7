e7fb387f0908c26237e3ac7da3e0ff28f38e3116
bijoy2017 <bijoy.sen@indusnet.co.in>
2019-05-22 11:59:10 +0530

    Model and Service implemented

--- getDataApi/src/app/app.component.ts
+++ getDataApi/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from './user.model';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'getDataApi';
+
+export class AppComponent implements OnInit {
+  user$: User[];
+  constructor(private dataservice: DataService) {}
+  ngOnInit() {
+    return this.dataservice.getUsers().subscribe(data => this.user$ = data);
+  }
 }
